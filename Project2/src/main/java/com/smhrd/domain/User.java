9451a69c49c854b67263c50980b5bd9e9f7bf6e0
smhrd <smhrd@59.0.249.7>
2022-05-16 12:43:21 +0900

    maven

---
+++ Project2/src/main/java/com/smhrd/domain/User.java
@@ -0,0 +1,15 @@
+package com.smhrd.domain;
+
+import java.sql.Timestamp;
+
+public class User {
+	private String id;
+	private String pw;
+	private String nick;
+	private String addr;
+	private Number manner;
+	private Timestamp date;
+	private String type;
+	private Number latitude;
+	private Number longitude;
+}
